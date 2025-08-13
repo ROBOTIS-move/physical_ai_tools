40fa3a085dc90c10debc70ca9fa28319b1f66612
Dongyun Kim <kdy@robotis.com>
2025-08-13 14:57:20 +0900

    fix: Update video deletion logic to correctly count removed videos based on camera subdirectories
    Signed-off-by: Dongyun Kim <kdy@robotis.com>

--- physical_ai_server/physical_ai_server/data_processing/data_editor.py
+++ physical_ai_server/physical_ai_server/data_processing/data_editor.py
@@ -471,14 +471,14 @@ class DataEditor:
                         with suppress(Exception):
                             tgt_video_file.unlink()
                         deleted_video = True
+                        removed_video_count = len(camera_subdirs) if camera_subdirs else 1
             else:
                 tgt_video_file = video_chunk_dir / f'{target_episode_stem}.mp4'
                 if tgt_video_file.exists():
                     with suppress(Exception):
                         tgt_video_file.unlink()
                     deleted_video = True
-        if deleted_video:
-            removed_video_count = 1
+                    removed_video_count = len(camera_subdirs) if camera_subdirs else 1            
 
         # 3. Delete images folder(s)
         images_root_dir = dataset_dir / 'images'

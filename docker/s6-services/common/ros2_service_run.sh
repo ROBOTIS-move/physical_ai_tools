a735b7e71c580d88ddf60eade22ac9820593877c
Hyungyu Kim <kimhg@robotis.com>
2026-01-09 11:05:55 +0900

    change location of socket volume mapping

--- docker/s6-services/common/ros2_service_run.sh
+++ docker/s6-services/common/ros2_service_run.sh
@@ -19,6 +19,8 @@ fi
 export ROS_DOMAIN_ID=${ROS_DOMAIN_ID:-30}
 export ROS_DISTRO=${ROS_DISTRO:-jazzy}
 export COLCON_WS=${COLCON_WS:-/root/ros2_ws}
+export PATH=/opt/venv/bin:${PATH}
+export PYTHONPATH=/opt/venv/lib/python3.12/site-packages:${PYTHONPATH}
 
 # Enable s6-overlay debug logging (set S6_VERBOSITY=1 for more verbose output)
 export S6_VERBOSITY=${S6_VERBOSITY:-1}
